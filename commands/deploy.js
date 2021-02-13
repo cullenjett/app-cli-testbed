0c5d8800b8252f2c005716c13d2412ed96bc6d8d
Cullen Jett <cullenjett@gmail.com>
2021-02-13 13:40:30 -0700

    pull gh access token from env and ping graphql endpoint

--- commands/deploy.js
+++ commands/deploy.js
@@ -1,3 +1,7 @@
-export const run = () => {
+import { fetchSampleRepo } from '../lib/github.js';
+
+export const run = async () => {
   console.log('Hello from deploy.js');
+  const res = await fetchSampleRepo();
+  console.log(res);
 };
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const [, , command] = process.argv;
 
 main(command);
 
 async function main(command) {
   const { run } = await import(`./commands/${command}.js`);
-  run();
+  await run();
 }
