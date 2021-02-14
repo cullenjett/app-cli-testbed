35a7e4d63b6108ae93eff9cb03f65bafa720f57c
Cullen Jett <cullenjett@gmail.com>
2021-02-14 08:03:59 -0700

    lots of hackery and wip 'deploy' command

--- commands/deploy.js
+++ commands/deploy.js
@@ -1,7 +1,68 @@
-import { createPullRequest } from '../lib/github.js';
+import {
+  createReleasePullRequest,
+  createRelease,
+  fetchTags,
+  getPullRequests,
+  mergePullRequest,
+  isPullRequestMerged,
+} from '../lib/github.js';
 
 export const run = async () => {
-  console.log('Hello from deploy.js');
-  const res = await createPullRequest();
-  console.log(res);
+  const existingTags = await fetchTags();
+  const tag = generateTag(existingTags);
+  console.log({ tag });
+
+  const pullNumber = await createReleasePullRequest({
+    title: `[Release] ${tag}`,
+    body: 'Lorem ipsum',
+  });
+  console.log({ pullNumber });
+
+  let isMerged = await isPullRequestMerged(1);
+  console.log({ isMerged });
+  while (!isMerged) {
+    await wait(2000);
+    isMerged = await isPullRequestMerged(pullNumber);
+    console.log({ isMerged });
+  }
+
+  const release = await createRelease(tag);
+  console.log({ release });
 };
+
+function generateTag(existingTags) {
+  const tag = 'v/' + todayString();
+
+  for (let i = 1; i < 100; i++) {
+    const versionedTag = i === 1 ? tag : `${tag}-${i}`;
+    const isAlreadyTagged = existingTags.includes(versionedTag);
+
+    if (!isAlreadyTagged) {
+      return versionedTag;
+    }
+  }
+
+  throw new Error('Unable to generate tag');
+}
+
+function todayString() {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = padZero(today.getMonth() + 1);
+  const day = padZero(today.getDate() - 1);
+
+  return `${year}.${month}.${day}`;
+}
+
+function padZero(num) {
+  if (num < 10) {
+    return '0' + num;
+  } else {
+    return String(num);
+  }
+}
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
